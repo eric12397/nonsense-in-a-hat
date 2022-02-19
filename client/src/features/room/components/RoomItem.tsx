b1122def3ea73ebcc7f2ce01953023d376a9a326
Eric Choi <eric12397@gmail.com>
2022-02-19 01:33:07 -0500

    rendered tiles for each room onto page

--- client/src/features/room/components/RoomItem.tsx
+++ client/src/features/room/components/RoomItem.tsx
@@ -4,12 +4,44 @@ import { Room } from '../interfaces/Room';
 interface RoomItemProps {
   key: string;
   room: Room;
+  host: any;
 };
 
-const RoomItem = ({ room }: RoomItemProps) => {
+const RoomItem = ({ room, host }: RoomItemProps) => {
   return (
-    <div className='p-3 rounded bg-white shadow-md'>
-      <p>{ room.name }</p>
+    <div className='bg-white shadow-md '>
+      <div className="flex items-center p-5">
+        <img
+          className="w-24 mr-8"
+          src={ host.avatar }
+        />
+
+        <div className='flex flex-col'>
+          <h3 className="font-semibold text-lg text-midnight">{ room.name }</h3>
+
+          <div className='flex flex-row mt-5'>
+            <div className='mr-20'>
+              <h5 className='font-semibold text-lg text-midnight'>Hosted By:</h5>
+              <p>{ host.name }</p>
+            </div>
+
+            <div className='mr-20'>
+              <h5 className='font-semibold text-lg text-midnight'>Capacity:</h5>
+              <p>{ room.currentPlayerCount } / { room.maxPlayersAllowed }</p>
+            </div>
+            
+            <div className='mr-20'>
+              <h5 className='font-semibold text-lg text-midnight'>Mode:</h5>
+              <p>{ room.gameMode }</p>
+            </div>
+
+            <div>
+              <h5 className='font-semibold text-lg text-midnight'>Status:</h5>
+              <p>{ room.isGameInProgress ? "In Progress" : "Waiting" }</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/features/room/components/RoomsList.tsx
+++ client/src/features/room/components/RoomsList.tsx
@@ -2,27 +2,49 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import RoomItem from './RoomItem';
 import { Room } from '../interfaces/Room';
+import rabbitPink from "../../../assets/rabbit-pink.svg";
+import rabbitBlue from "../../../assets/rabbit-blue.svg";
+
+const initialRoomData1: Room = {
+  id: "123",
+  name: "Naansense",
+  host: { name: "Zekey Education", avatar: rabbitPink  },
+  currentPlayerCount: 1,
+  maxPlayersAllowed: 8,
+  gameMode: "Classic",
+  isGameInProgress: false,
+}
+const initialRoomData2: Room = {
+  id: "123",
+  name: "Resume Workshop",
+  host: { name: "Gordi", avatar: rabbitBlue },
+  currentPlayerCount: 4,
+  maxPlayersAllowed: 8,
+  gameMode: "Classic",
+  isGameInProgress: true,
+}
 
 const RoomsList = () => {
-  const [rooms, setRooms] = useState<Room[]>();
+  const [rooms, setRooms] = useState<Room[]>([initialRoomData1,initialRoomData2,initialRoomData1]);
 
   useEffect(() => {
     axios
-      .get('http://localhost:8000/rooms')
+      .get<Room[]>('http://localhost:8000/rooms')
       .then(response => {
         setRooms(response.data);
       })
   }, []);
 
   return (
-    <>
+    <div className='grid grid-cols-1 divide-y divide-silver'>
       { rooms && rooms.map(room => (
         <RoomItem 
           key={ room.id }
           room={ room }
+          host={ room.host }
         ></RoomItem> 
       ))}
-    </>
+    </div>
   );
 }
 export default RoomsList;
